60ba7d2ba4a9af3507c0ee2e8377fe996773274a
Kevin Valdek <kevinvaldek@gmail.com>
2021-06-01 20:54:23 +0200

    node example up to date.

--- nodejs/create_auth_token.js
+++ nodejs/create_auth_token.js
@@ -1,10 +1,11 @@
 var fs = require('fs')
 var jwt = require('jsonwebtoken')
+var uuid4 = require('uuid4')
 var request = require('request');
 
 // Get API details
 var ACCOUNT_API_CONFIG = {
-    version: 2,
+    version: 1,
     base_url: 'https://sandbox.api.high-mobility.com/v1',
     api_key: '6456a189-7c39-4343-b02a-3ee4c3a63142',
     private_key: "-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIEYhNYsWg+Bc8Zt7d9IjKjSeQ+O4NiGaBbjP2eIOMF+ToAoGCCqGSM49\nAwEHoUQDQgAEeCm3pl4WpevWJw/fO0cCjwh2pntgw3Xw7TG6Frrhep/y3mvU18Ks\nwcCucERfkbY9AkPeTXseFC7DsKsexrMk2A==\n-----END EC PRIVATE KEY-----\n\n"
@@ -13,7 +14,7 @@ var iat = Math.round(Date.now()/1000);
 
 var priv = Buffer.from(ACCOUNT_API_CONFIG.private_key, 'utf8')
 
-var payload = { 'ver': ACCOUNT_API_CONFIG.version, iss: ACCOUNT_API_CONFIG.api_key, 'aud': ACCOUNT_API_CONFIG.base_url, 'iat': iat };
+var payload = { 'ver': ACCOUNT_API_CONFIG.version, iss: ACCOUNT_API_CONFIG.api_key, 'aud': ACCOUNT_API_CONFIG.base_url, 'iat': iat, 'jti': uuid4() };
 var token = jwt.sign(payload, priv, { algorithm: 'ES256' })
 
 request.post(
